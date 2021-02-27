642643a07171f6dd55715484a79db4327691e5f2
larsm <lars.mensch@gmail.com>
2021-02-27 17:37:55 +0100

    Main Methode verändert. -Game-Button erstellt -Game-Button Grroeße verändert

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.concurrent.Flow;
 
 public class Main {
 
@@ -10,10 +11,17 @@ public class Main {
         JDialog jDialog = new JDialog();
         jDialog.setTitle("Hallo");
         jDialog.setSize(800,800);
+
+     //  JPanel panel = new JPanel(new GridLayout(100, 100));
+
+        JButton button = new JButton("Game of Life");
+        button.setPreferredSize(new Dimension(100, 100));
+        jDialog.getContentPane().setLayout(new FlowLayout());
+        jDialog.getContentPane().add(button);
+       // jDialog.add(new JButton("Game"), BorderLayout.CENTER);
+
+      //  jDialog.add(button);
         jDialog.setVisible(true);
-
-
-
 
 
 
