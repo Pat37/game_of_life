2d3787d8e1d13f7184ee4e5bf784a6a22e9874c9
Patrick <patrick.schnuettgen@outlook.de>
2021-02-27 16:29:14 +0100

    JDialog Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
 
     public static void main(String[] args){
 
-        System.out.println("Fotze");
+
+        JDialog jDialog = new JDialog();
+        jDialog.setTitle("Hallo");
+        jDialog.setSize(800,800);
+        jDialog.setVisible(true);
+
+
+
+
+
+
+
     }
 }
