06d9079e8e68f9cf7d425b24bc8bfa3b7219f59b
larsm <lars.mensch@gmail.com>
2021-02-28 10:19:12 +0100

    zweite

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,19 +8,28 @@ public class Main {
     public static void main(String[] args){
 
 
+
         JDialog jDialog = new JDialog();
         jDialog.setTitle("Hallo");
         jDialog.setSize(800,800);
 
-     //  JPanel panel = new JPanel(new GridLayout(100, 100));
+        JPanel panel = new JPanel(new GridLayout(5, 1));
+        JButton button = new JButton("Game");
+        jDialog.add(panel);
+       //panel.add(new JButton(("Centered")), new GridBagConstraints());
 
-        JButton button = new JButton("Game of Life");
+
+
+
+      /*
         button.setPreferredSize(new Dimension(100, 100));
         jDialog.getContentPane().setLayout(new FlowLayout());
         jDialog.getContentPane().add(button);
+       // jDialog.add(button);
        // jDialog.add(new JButton("Game"), BorderLayout.CENTER);
 
-      //  jDialog.add(button);
+      //  jDialog.add(button);*/
+
         jDialog.setVisible(true);
 
 
